ac8f9294976d0ce2847dc0cc58849a9b279dcd64
phy <phy>
2019-12-12 18:29:05 +0800

    init

--- .umirc.ts
+++ .umirc.ts
@@ -1,40 +1,42 @@
-import { IConfig } from 'umi-types';
+import { IConfig } from 'umi-types'
 
 // ref: https://umijs.org/config/
-const config: IConfig =  {
-  treeShaking: true,
-  routes: [
-    {
-      path: '/',
-      component: '../layouts/index',
-      routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    }
-  ],
-  plugins: [
-    // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: { webpackChunkName: true },
-      title: 'char',
-      dll: true,
-      locale: {
-        enable: true,
-        default: 'en-US',
-      },
-      routes: {
-        exclude: [
-          /models\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
-      },
-    }],
-  ],
+const config: IConfig = {
+	treeShaking: true,
+	sass: {},
+	routes: [
+		{
+			path: '/',
+			component: '../layouts/index',
+			routes: [ { path: '/', component: '../pages/index' } ]
+		}
+	],
+	plugins: [
+		// ref: https://umijs.org/plugin/umi-plugin-react.html
+		[
+			'umi-plugin-react',
+			{
+				antd: true,
+				dva: true,
+				dynamicImport: { webpackChunkName: true },
+				title: 'char',
+				dll: true,
+				locale: {
+					enable: true,
+					default: 'en-US'
+				},
+				routes: {
+					exclude: [
+						/models\//,
+						/services\//,
+						/model\.(t|j)sx?$/,
+						/service\.(t|j)sx?$/,
+						/components\//
+					]
+				}
+			}
+		]
+	]
 }
 
-export default config;
+export default config
--- typings.d.ts
+++ typings.d.ts
@@ -1,2 +1,5 @@
-declare module '*.css';
-declare module "*.png";
+declare module '*.css'
+declare module '*.scss'
+declare module '*.less'
+declare module '*.styl'
+declare module '*.png'
