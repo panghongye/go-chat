d5c846168929020df7ebc7699b30e39f9425e918
phy <phy>
2019-12-14 18:38:56 +0800

    -

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,10 @@
-import withRouter from 'umi/withRouter';
-import { observer } from "mobx-react"
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import withRouter from 'umi/withRouter'
+import { observer } from 'mobx-react'
 import React from 'react'
 import store from './models/index'
 
-
-export function rootContainer(container: JSX.Element) {
-	return (
-		<Router>
-			{/* <Root /> */}
-			{container}
-		</Router>
-	)
-}
-
-const Root = withRouter((props) => {
-	const pathname = props.location.pathname
-	return (
-		<>
-			<Switch>
-				<Route path="/login" component={require('./pages/login').default}></Route>
-				<Route path="/setting">setting</Route>
-				<Route path="/" component={require('./pages/index').default} />
-			</Switch>
-		</>
-	)
+const Root = observer((props) => {
+	return props.children
 })
+
+export const rootContainer = (container: JSX.Element) => <Root>{container}</Root>
